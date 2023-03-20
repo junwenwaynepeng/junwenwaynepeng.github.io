852363d9b98090e9b5a92763b10b1667832121bb
Wayne Peng <junwen.wayne.peng@gmail.com>
2023-03-21 00:11:58 +0800

    update js

---
+++ assets/js/chatgpt/2023-03-20-2.js
@@ -0,0 +1,52 @@
+let open_ai_response;
+
+async function openai_test() {
+  const apiKey = document.getElementById("api-key").value;
+  const answer = document.getElementById('1-3').value;
+  var url = "https://api.openai.com/v1/completions";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Bearer " + apiKey);
+
+  xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4) {
+        open_ai_response = JSON.parse(xhr.responseText);
+        if (open_ai_response.choices) {
+          const generatedText = open_ai_response.choices[0].text;
+          // Display the response in the result-box div
+          document.getElementById("result-box-1-3").innerHTML = generatedText;
+        } else {
+          console.error('Error: No "choices" property in OpenAI response.');
+        }
+        // Display the response in the result-box div
+        document.getElementById("result-box-1-3").innerHTML = generatedText;
+     }};
+
+  var data = {
+    "prompt": `
+Decide whether a student\'s response is essentially correct. 
+
+The followings are correct answers : 
+* We want to use second order test
+* If we can show that D(x,y)<0, then we know (x,y) is a minimal of the surface.
+
+The followings are not accepted answer:
+* We have to use D(x,y) to determine if (x,y) is maximal or minimal.
+
+student:` + answer + `
+
+Is correct:`,
+    "temperature": 0,
+    "max_tokens": 60,
+    "top_p": 1,
+    "frequency_penalty": 0.5,
+    "presence_penalty": 0,
+    "model": "text-davinci-003"
+  };
+  data = JSON.stringify(data)
+  console.log(data)
+  xhr.send(data);
+}
