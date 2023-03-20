21bd03088577978400ff97fb0cd118b525433493
Wayne Peng <junwen.wayne.peng@gmail.com>
2023-03-20 23:44:07 +0800

    remove script

--- assets/js/chatgpt/2023-03-20-1.js
+++ assets/js/chatgpt/2023-03-20-1.js
@@ -1,33 +1,32 @@
-<script>
-  let open_ai_response;
+let open_ai_response;
 
-  async function openai_test() {
-    const apiKey = document.getElementById("api-key").value;
-    const answer = document.getElementById('1-2').value;
-    var url = "https://api.openai.com/v1/completions";
+async function openai_test() {
+  const apiKey = document.getElementById("api-key").value;
+  const answer = document.getElementById('1-2').value;
+  var url = "https://api.openai.com/v1/completions";
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
 
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("Authorization", "Bearer " + apiKey);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Bearer " + apiKey);
 
-    xhr.onreadystatechange = function () {
-       if (xhr.readyState === 4) {
-          open_ai_response = JSON.parse(xhr.responseText);
-          if (open_ai_response.choices) {
-            const generatedText = open_ai_response.choices[0].text;
-            // Display the response in the result-box div
-            document.getElementById("result-box").innerHTML = generatedText;
-          } else {
-            console.error('Error: No "choices" property in OpenAI response.');
-          }
+  xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4) {
+        open_ai_response = JSON.parse(xhr.responseText);
+        if (open_ai_response.choices) {
+          const generatedText = open_ai_response.choices[0].text;
           // Display the response in the result-box div
           document.getElementById("result-box").innerHTML = generatedText;
-       }};
+        } else {
+          console.error('Error: No "choices" property in OpenAI response.');
+        }
+        // Display the response in the result-box div
+        document.getElementById("result-box").innerHTML = generatedText;
+     }};
 
-    var data = {
-      "prompt": `
+  var data = {
+    "prompt": `
 Decide whether a student\'s response is essentially correct. 
 
 The followings are correct answers : 
@@ -43,15 +42,14 @@ The followings are not accepted answer:
 student:` + answer + `
 
 Is correct:`,
-      "temperature": 0,
-      "max_tokens": 60,
-      "top_p": 1,
-      "frequency_penalty": 0.5,
-      "presence_penalty": 0,
-      "model": "text-davinci-003"
-    };
-    data = JSON.stringify(data)
-    console.log(data)
-    xhr.send(data);
-  }
-</script>
+    "temperature": 0,
+    "max_tokens": 60,
+    "top_p": 1,
+    "frequency_penalty": 0.5,
+    "presence_penalty": 0,
+    "model": "text-davinci-003"
+  };
+  data = JSON.stringify(data)
+  console.log(data)
+  xhr.send(data);
+}
