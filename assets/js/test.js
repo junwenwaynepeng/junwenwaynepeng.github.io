93639bf30d118bf8ed133d98cc5140c15fe7d696
Wayne Peng <junwen.wayne.peng@gmail.com>
2023-03-25 00:19:57 +0800

    test-3

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,6 +1,6 @@
 
 
-fetch('./assets/js/chatgpt/template')
+fetch('./assets/js/chatgpt/template.txt')
   .then(response => response.text())
   .then(text => {
     document.getElementById('test').innerHTML = text;
