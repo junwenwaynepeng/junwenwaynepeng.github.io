27f79834fc278265931ab3c7fd19fcc25d1f704b
Wayne Peng <junwen.wayne.peng@gmail.com>
2023-03-25 00:59:12 +0800

    test-10

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,40 +1,47 @@
-let open_ai_response_0;
+// Fetch the template text from file
+fetch('../assets/js/chatgpt/template.txt')
+  .then(response => response.text())
+  .then(text => {
+    // Once the template is loaded, define the openai_0 function
+    function openai_0() {
+      const apiKey = document.getElementById("api-key").value;
+      const answer = document.getElementById('1-1').value;
+      var url = "https://api.openai.com/v1/completions";
 
-async function openai_0() {
-  const apiKey = document.getElementById("api-key").value;
-  const answer = document.getElementById('1-1').value;
-  var url = "https://api.openai.com/v1/completions";
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", url);
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", url);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("Authorization", "Bearer " + apiKey);
 
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("Authorization", "Bearer " + apiKey);
-
-  xhr.onreadystatechange = function () {
-     if (xhr.readyState === 4) {
-        open_ai_response_0 = JSON.parse(xhr.responseText);
-        console.log(open_ai_response_0);
-        if (open_ai_response_0.choices) {
-          const generatedText = open_ai_response_0.choices[0].text;
-          // Display the response in the result-box div
-          document.getElementById("result-box-1-1").innerHTML = generatedText;
-        } else {
-          console.error('Error: No "choices" property in OpenAI response.');
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          open_ai_response_0 = JSON.parse(xhr.responseText);
+          console.log(open_ai_response_0);
+          if (open_ai_response_0.choices) {
+            const generatedText = open_ai_response_0.choices[0].text;
+            // Display the response in the result-box div
+            document.getElementById("result-box-1-1").innerHTML = generatedText;
+          } else {
+            console.error('Error: No "choices" property in OpenAI response.');
+          }
         }
-     }
-  };
+      };
 
-  var data = {
-    "prompt": await fetch('../assets/js/chatgpt/template.txt').then(response => response.text()),
-    "temperature": 0,
-    "max_tokens": 60,
-    "top_p": 1,
-    "frequency_penalty": 0.5,
-    "presence_penalty": 0,
-    "model": "text-davinci-003"
-  };
-  data = JSON.stringify(data)
-  console.log(data)
-  xhr.send(data);
-}
+      var data = {
+        "prompt": text,
+        "temperature": 0,
+        "max_tokens": 60,
+        "top_p": 1,
+        "frequency_penalty": 0.5,
+        "presence_penalty": 0,
+        "model": "text-davinci-003"
+      };
+      data = JSON.stringify(data);
+      console.log(data);
+      xhr.send(data);
+    }
+
+    // Assign the openai_0 function to the onclick event of the button
+    document.getElementById("submit-1-1").onclick = openai_0;
+  });
