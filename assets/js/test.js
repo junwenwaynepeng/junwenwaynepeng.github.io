adff83515c8a40910a6eec707e506d76ce920ef4
wayne peng <junwen.wayne.peng@gmail.com>
2023-03-27 19:05:36 +0800

    gpt-test-25

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,12 +1,12 @@
 // Fetch the template text from file
 function gpt(id){
-  fetch('../assets/js/chatgpt/${id}.txt')
+  fetch('../assets/js/chatgpt/{id}.txt'.replace('{id}',id))
     .then(response => response.text())
     .then(text => {
       // Once the template is loaded, define the openai_0 function
       console.log("answer-${id}")
       const apiKey = document.getElementById("api-key").value;
-      const answer = document.getElementById("answer-${id}").value;
+      const answer = document.getElementById("answer-{id}".replace('{id}',id)).value;
       const url = "https://api.openai.com/v1/completions";
 
       var xhr = new XMLHttpRequest();
@@ -22,7 +22,7 @@ function gpt(id){
           if (open_ai_response_0.choices) {
             const generatedText = open_ai_response_0.choices[0].text;
             // Display the response in the result-box div
-            document.getElementById("result-box-${id}").innerHTML = generatedText;
+            document.getElementById("result-box-{id}".replace('{id}',id)).innerHTML = generatedText;
           } else {
             console.error('Error: No "choices" property in OpenAI response.');
           }
