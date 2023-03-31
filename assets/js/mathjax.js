96a334af135614d14a6f6ed79eb70b99f4d0126b
wayne peng <junwen.wayne.peng@gmail.com>
2023-03-31 11:56:41 +0800

    update head

---
+++ assets/js/mathjax.js
@@ -0,0 +1,13 @@
+  MathJax.Hub.Config({
+    jax: ["input/TeX", "output/HTML-CSS"],
+    extensions: ["tex2jax.js"],
+    "HTML-CSS": { preferredFont: "TeX", availableFonts: ["STIX","TeX"] },
+    tex2jax: { 
+      inlineMath: [ ["$", "$"], ["\\(","\\)"] ], 
+      displayMath: [ ["\[", "\]"], ["$$","$$"] ], 
+      processEscapes: true, 
+      ignoreClass: "tex2jax_ignore|dno" },
+    TeX: { 
+      noUndefined: { attributes: { mathcolor: "red", mathbackground: "#FFEEEE", mathsize: "90%" } } },
+    messageStyle: "none"
+  });
