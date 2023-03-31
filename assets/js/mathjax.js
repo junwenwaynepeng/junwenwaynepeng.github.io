37081b8c5df7274971af987b25e276347f08f348
wayne peng <junwen.wayne.peng@gmail.com>
2023-03-31 16:16:44 +0800

    fix mathjax-14

--- assets/js/mathjax.js
+++ assets/js/mathjax.js
@@ -1,20 +1,11 @@
-MathJax.Hub.Config({
-  jax: ["input/TeX", "output/HTML-CSS"],
-  extensions: ["tex2jax.js"],
-  "HTML-CSS": { preferredFont: "TeX", availableFonts: ["STIX","TeX"] },
-  tex2jax: { 
-    inlineMath: [ ["$", "$"], ["\\(","\\)"] ], 
-    displayMath: [ ["\[", "\]"], ["$$","$$"] ], 
-    processEscapes: true, 
-    ignoreClass: "tex2jax_ignore|dno" },
-  TeX: { 
-    noUndefined: { attributes: { mathcolor: "red", mathbackground: "#FFEEEE", mathsize: "90%" } } },
-  messageStyle: "none"
-});
-
-/*if (window.MathJax) {
-  MathJax.Hub.Queue(
-    ["resetEquationNumbers",MathJax.InputJax.TeX],
-    ["Typeset",MathJax.Hub]
-  );
-}*/
+MathJax = {
+  loader: {
+    load: ['[tex]/mathtools']
+  },
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
+  packages: {
+    '[+]': ['mathtools']
+  },
+};
