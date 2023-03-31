6cdcb77891efdd54b5beadc8f09b61cdbdbdc645
wayne peng <junwen.wayne.peng@gmail.com>
2023-03-31 16:45:44 +0800

    fix mathjax-20

--- assets/js/mathjax.js
+++ assets/js/mathjax.js
@@ -9,3 +9,10 @@ window.MathJax = {
     '[+]': ['mathtools']
   },
 };
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
