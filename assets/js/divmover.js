57121a9c5678bb33fe6a5d341aef74463b1d98c3
Wayne Peng <junwen.wayne.peng@gmail.com>
2023-04-04 18:17:53 +0800

    dev question display-32 try divmover.j

--- assets/js/divmover.js
+++ assets/js/divmover.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var questions = document.querySelectorAll(".question");
+  console.log(questions)
   for (var i = 0; i < questions.length; i++) {
     var pk = questions[i].id;
+    console.log(pk)
     var target = document.getElementById("question-" + pk);
     if (target) {
       target.innerHTML = questions[i].innerHTML;
